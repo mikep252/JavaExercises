027cdea8f8b7d73d6ff0a47e9e79fdbb19d622f3
mikep252 <mikep252@wp.pl>
2019-09-23 22:29:25 +0200

    Add files via upload

---
+++ Inheritance/src/com/company/Main.java
@@ -0,0 +1,14 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+	    Animal animal = new Animal("Animal", 1, 1,5,5);
+	    Dog dog = new Dog("Yorkie",8,202,4,1,20,4,10,10,"Long air");
+	    //dog.eat();
+	    ///dog.walk();
+	    //dog.run();
+dog.move(500);
+
+    }
+}
